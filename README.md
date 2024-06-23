4a3f362453d4390bedc5e704832c264c7daf1267
Isabelle <isatkins@microsoft.com>
2024-06-23 18:54:15 +0100

    concurrent code, with tests and readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Overview
+This is a Rust implementation of a scraper which, given a root url of format https://<domainname>, prints a HashMap where the key is a URL, and the value is all linked URLs from that URL page.
+
+# Implementation details
+It starts with a root URL. It sends a GET request to that URL, and parses the HTML from the response, extracting all linked URLs from the webpage. It checks if those links are either relative paths (starting "/"), or share the domain prefix - if so, it stores these child URLs in the hashmap entry for the root URL. It also stores each URL in a separate vector, which stores all URLs which have not yet been visited.
+It iterates through this vector of urls_to_visit, and recursively does the above on each entry. It does this repeatedly, until all URLs have been visited.
+
+To improve the speed of the programme, I spawned new tokio tasks each time we want to crawl over a new URL. I needed to set a maximum for how many tasks were spawned to avoid hitting an error when attempting to open too many TCP connections at the same time. I set this value to 100- exploring what it should be exactly is identified in the Areas for Improvement section below.
+
+I edited the headers of the GET request to get around Cloudflare's blocking of requests that look like they come from crawlers- otherwise, we'd get 403s for our GET requests. I did this by setting the User Agent to something that looked like a "real user".
+
+Hyperlinks in the HTML could be of two forms: absolute paths or relative paths. I've added support for both. Note, this crawler only explores paths which have the same subdomain.
+
+
+# How to use
+1. Clone the repository
+1. From the root of the repository, set your 
+```console
+cargo run `target_url`
+```
+
+e.g 
+
+```console
+cargo run https://google.com
+```
+
+# Areas for improvement
+
+This was a timeboxed exercise and I prioritised making the crate more performant by adding concurrency over a few other items. I've listed those items below, which should be improved.
+
+- Handling 202s: A 202 tells us the request is accepted, but is in a queue to be processed. Currently, if we receive a 202, we move on, and that URL is not written into our HashMap. That might mean some entries are missed (best case), or worst case: if we receive a 202 for the *first* GET request for the root URL, then the programme will return immediately! So no other URLs are visited and our return result is empty. Currently we print a warning message if this worst case is hit, so that the user knows to retry shortly. The user can rerun manually as a work-around. Nevertheless, this should be improved on. Perhaps for instance, we should check if the location field in the 202 response headers points to the location we'll be able to find the content soon. Or perhaps we retry.
+
+- Refactor for better encapsulation. I don't like that crawl_individual_url is a free function that accesses/modifies the Crawler struct's state - that's bad encapsulation. I did this as a work around: when trying to spawn threads which directly accessed state by 'self.x', I was getting Compiler complaints about Self not being Copy and thread safe. This needs to be improved upon.
+
+- Error handling: Currently, there's a bunch of .unwrap() calls. Most of these are safe (ie we know we'll only get an Ok() or a Some()) but I haven't checked all of them. If I had more time, I'd check them all and improve the error handling. I don't want one failed GET request to a given URL to cause the whole programme to fail. Instead, it prints the error hit, and continues. This would result in an incomplete set. 
+
+- I used a synchronous Mutex. This blocks threads for a small amount of time whilst the lock is held. This is fine and standard, on the assumption that lock contention isn't too high. I haven't played around with what "too high" looks like, but this crate spawns 100 concurrent threads trying to obtain the lock. To improve on this, investigate how many threads we should spawn to optimise speed whilst not raising lock contention.
+
+- More testing: I added some noddy tests, but the coverage is lacking.
+# Code iterations and resulting time improvements
+1. For the first pass, I wrote synchronous single-threaded code which would check each URL in turn.
+    time taken: 306s
+
+1. For a second pass, I spawned a tokio task for each URL in the vector of urls_to_visit, so that they could be searched concurrently. I wrapped the shared state objects in Arc<Mutexes> so that they could be shared across threads safely.
+    time taken: 38s
+
+2. For a third pass, I realised I was missing any relative paths - so I added support for relative paths, which meant our net was catching about five 6 times the number of paths. So the time went up.
+    time taken: 59s
+
--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,256 @@
-use reqwest::Client;
+use reqwest::{Client, Response};
 use scraper::{Html, Selector};
 use std::collections::HashMap;
 use std::env::args;
-use std::time::SystemTime;
+use std::process::exit;
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::{Duration, SystemTime};
+use tokio::sync::Semaphore;
+use tokio::task::JoinSet;
 
 #[tokio::main]
 async fn main() {
+    // Mark the start time of the crawl, so we can measure how long it takes.
     let now = SystemTime::now();
     let args: Vec<String> = args().collect();
-    //let url = &args[1].parse::<hyper::Uri>().unwrap();
+    if args.len() != 2 {
+        println!("Input error: Incorrect number of arguments provided, precisely one argument should be given.");
+        exit(1)
+    }
+    let origin_url = &args[1];
 
     let client = Client::new();
-    let origin_url = "https://monzo.com/";
-    //Many websites are protected by Cloudflare, which detects if a request is coming from a real user or a web scraper (that's us!).
+    // Send GET request to check if server is returning 200s.
+    // Many websites are protected by Cloudflare, which detects if a request is coming from a real user or a web scraper (that's us!).
     // If it detects that the request is coming from a web scraper, it will block the request and return a 403 Forbidden status code.
     // To avoid this, we can set the User-Agent header to a value that is commonly used by web browsers.
     // Interestingly, when I started writing this programme in python I didn't run into this problem - so the python requests library must be handling this under the covers!
-    let mut res = client.get(origin_url).header("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148").send().await.unwrap();
+    let res = client.get(origin_url).header("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148").send().await.unwrap();
     println!("Status for {}: {}", origin_url, res.status());
-    let mut crawler = Crawler::new(origin_url.to_string());
-    crawler.crawl().await;
-    match now.elapsed() {
-        Ok(elapsed) => {
-            // it prints '2'
-            println!("{}", elapsed.as_secs());
-        }
-        Err(e) => {
-            // an error occurred!
-            println!("Error: {e:?}");
-        }
+    if res.status() == reqwest::StatusCode::ACCEPTED {
+        println!("Target server is under load and returning 202s which we don't currently handle. Retry shortly.")
     }
+
+    let crawler = Crawler::new(origin_url.to_string());
+    // Crawl the server.
+    crawler.crawl_whole().await;
+
+    // Print out how long the crawler took.
+    println!(
+        "{:#?}",
+        now.elapsed()
+            .expect("Error calculating time taken to crawl.")
+            .as_secs()
+    )
 }
 
 struct Crawler {
+    // The original URL passed in as an argument to cargo run
+    root: String,
+    // An HTTP client, to be reused for each GET request.
     //Note don't need to wrap client in an Arc as the Client type already uses an Arc internally, therefore can be safely shared between threads
     client: Client,
-    urls_to_visit: Vec<String>,
-    urls: HashMap<String, Vec<String>>,
+    // A store of outstanding URLs that need to be crawled
+    urls_to_visit: Arc<Mutex<Vec<String>>>,
+    // A key value store of all URLs found on the page of a given URL
+    urls: Arc<Mutex<HashMap<String, Vec<String>>>>,
 }
 
 impl Crawler {
     fn new(root: String) -> Crawler {
         Crawler {
+            root: root.clone(),
             client: Client::new(),
-            urls_to_visit: vec![root],
-            urls: HashMap::new(),
+            urls_to_visit: Arc::new(Mutex::new(vec![root])),
+            urls: Arc::new(Mutex::new(HashMap::new())),
         }
     }
 
-    async fn crawl(&mut self) {
-        while !self.urls_to_visit.is_empty() {
-            let path = self.urls_to_visit.pop().unwrap();
-            let res = self.client.get(path.clone()).header("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148").send().await.unwrap();
-            let text = res.text().await.unwrap();
-            let document = Html::parse_document(&text);
-            // The a tag in html is used to define a hyperlink between from one page to another
-            let selector = Selector::parse("a").unwrap();
-            for a_tag in document.select(&selector) {
-                let url = match a_tag.value().attr("href") {
-                    Some(url) => url.to_string(),
-                    None => continue,
-                };
-                if url.starts_with("https://monzo.com") {
-                    if !self.urls.contains_key(&url) {
-                        self.urls_to_visit.push(url.clone());
-                    }
-                    self.urls.entry(path.clone()).or_insert(vec![]).push(url);
-                }
+    /// This function is responsible for crawling the whole website. It does this by spawning a number of tasks, each of which crawls an individual URL.
+    /// The function uses a while loop to repeatedly spawn tasks until there are no more URLs to visit.
+    /// It uses a JoinSet to wait for all the tasks to complete.
+    /// It uses a semaphore to limit the number of concurrent tasks that are spawned to avoid hitting the maximum number of open sockets.
+    /// It prints out a map, with each URL as a key, and the value as a list of URLs that it links to.
+    async fn crawl_whole(self) {
+        // There's an upper limit of how many outgoing TCP connections we can open at a given time (limited by how many sockets we can open)- if we try and spawn
+        // more tokio tasks than this limit, we'll hit an error for having too many files open. Therefore, we limit the number of concurrent tasks spawned
+        // using a semaphore. The semaphore is initialized with the number of concurrent tasks we want to allow, and each time we spawn a task, we acquire a permit
+        let sem = Arc::new(Semaphore::new(100));
+
+        // JoinSet is a helper struct that allows us to spawn a number of tasks and then wait for them all to complete.
+        let mut set = JoinSet::new();
+        // Spawn an "initial task" that will sleep for 1 second. This is necessary so that we can initially enter the while loop immediately below.
+        set.spawn(async {
+            sleep(Duration::from_secs(1));
+        });
+
+        // Enter a while loop that will continue until there are no more URLs to visit. For that to be true, the `urls_to_visit` vector must be empty and
+        // all the tasks spawned must have completed. Use the `join_next` method on the JoinSet to check if there are any tasks that have not yet completed.
+        while set.join_next().await.is_some() || !self.urls_to_visit.lock().unwrap().is_empty() {
+            while !self
+                .urls_to_visit
+                .lock()
+                .expect("Count not obtain lock")
+                .is_empty()
+            {
+                // Aquire a permit from the semaphore, which will block if the number of concurrent tasks has reached the limit.
+                let permit = Arc::clone(&sem).acquire_owned().await;
+
+                // Pop off a URL from the urls_to_visit
+                let path = self
+                    .urls_to_visit
+                    .clone()
+                    .lock()
+                    .expect("Count not obtain lock")
+                    .pop()
+                    .unwrap();
+
+                // Clone the necessary variables so that they can be moved into the spawned task.
+                let urls_to_visit = self.urls_to_visit.clone();
+                let urls = self.urls.clone();
+                let client = self.client.clone();
+                let root = self.root.clone();
+
+                // Spawn a task to crawl the given URL
+                set.spawn(async move {
+                    // Obtain a permit - this will block if we've reached the upper limit of how many concurrent tasks we can have
+                    let _permit = permit;
+                    crawl_individual_url(path, client.clone(), urls, urls_to_visit, root.clone())
+                        .await;
+                });
             }
         }
+        println!("urls {:#?}", self.urls.lock().unwrap());
+        println!("length of urls {:#?}", self.urls.lock().unwrap().len());
+    }
+}
 
-        println!("{:#?}", self.urls);
-        for url in self.urls_to_visit.iter() {
-            println!("{}", url);
+/// This function is responsible for crawling an individual URL. It sends a GET request to the URL, and then parses the HTML response.
+/// It then extracts all the URLs from the HTML response, and adds them to the `urls_to_visit` vector if they are not already present.
+/// It also adds the URL to the `urls` hashmap, which maps a URL to all the URLs that it links to.
+async fn crawl_individual_url(
+    path: String,
+    client: Client,
+    urls: Arc<Mutex<HashMap<String, Vec<String>>>>,
+    urls_to_visit: Arc<Mutex<Vec<String>>>,
+    root: String,
+) {
+    // If the path given is relative to the root (ie it starts with "/"), prepend it with the root
+    let url = if path.starts_with("/") {
+        root.clone() + &path
+    } else {
+        path.clone()
+    };
+
+    // Make get request to provided URL
+    let res = match client.get(url.clone()).header("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E147").send().await {
+        Ok(res) => res,
+        Err(e) => {
+            println!("Error: {:#?}", e);
+            return;
         }
+    };
+    // If the status code is not 200, return early
+    match res.status() {
+        reqwest::StatusCode::OK => (),
+        _ => return,
+    }
+    // Extract the URLs from the HTML of the response
+    let extracted_urls = parse_resp_to_urls(res).await;
+
+    for child_url in extracted_urls.iter() {
+        // Filter for URLs that have the same domain name as the root URL passed in
+
+        if child_url.starts_with(&root) || child_url.starts_with("/") {
+            // Add the URL to the urls_to_visit object, unless its already present
+            if !urls
+                .lock()
+                .expect("Could not obtain lock")
+                .contains_key(child_url)
+            {
+                urls_to_visit
+                    .lock()
+                    .expect("Could not obtain lock")
+                    .push(child_url.clone());
+            }
+            // Store the URL as a child URL for the parent URL in the urls object
+            urls.lock()
+                .expect("Could not obtain lock")
+                .entry(path.clone())
+                .or_insert(vec![])
+                .push(child_url.to_owned());
+        }
+    }
+}
+
+/// Takes a GET response, extracts the HTML from the text, filters for a tags (where hyperlinks are specified in HTML),
+/// filters for those which contain hrefs, and returns a vector of URLs as strings.
+async fn parse_resp_to_urls<'a>(res: Response) -> Vec<String> {
+    let mut urls = vec![];
+    let text = res.text().await.unwrap();
+    let document = Html::parse_document(&text);
+    // Filter for HTML a tags, which define child hyperlinks
+    let selector = Selector::parse("a").unwrap();
+    for a_tag in document.select(&selector) {
+        let url = match a_tag.value().attr("href") {
+            Some(url) => url.to_string(),
+            None => continue,
+        };
+        urls.push(url);
+    }
+    urls
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    /// Test that given a single URL, the correct child URLs are appended to the urls_to_visit and urls objects correctly.
+    /// Note for this test and all others, we use the test website scrapethissite.com
+    async fn test_single_url_crawl() {
+        let client = Client::new();
+        let path = "https://www.scrapethissite.com/".to_string();
+        let urls = Arc::new(Mutex::new(HashMap::new()));
+        let urls_to_visit = Arc::new(Mutex::new(vec![]));
+        let root = "https://www.scrapethissite.com/".to_string();
+        crawl_individual_url(path, client, urls.clone(), urls_to_visit.clone(), root).await;
+        let expected = vec![
+            "/".to_string(),
+            "/pages/".to_string(),
+            "/lessons/".to_string(),
+            "/faq/".to_string(),
+            "/login/".to_string(),
+            "/pages/".to_string(),
+            "/lessons/".to_string(),
+        ];
+        assert_eq!(
+            *urls_to_visit.lock().expect("couldn't obtain lock"),
+            expected.clone(),
+        );
+        assert_eq!(
+            *urls.lock().expect("couldn't obtain lock")["https://www.scrapethissite.com/"],
+            expected
+        );
+    }
+
+    #[tokio::test]
+    /// Test that for a given webpage, we successfully extract the hrefs.
+    async fn test_html_parsing() {
+        let expected = vec![
+            "/".to_string(),
+            "/pages/".to_string(),
+            "/lessons/".to_string(),
+            "/faq/".to_string(),
+            "/login/".to_string(),
+            "/pages/".to_string(),
+            "/lessons/".to_string(),
+        ];
+        let client = Client::new();
+        let path = "https://www.scrapethissite.com/".to_string();
+        let res =  client.get(path.clone()).header("User-Agent", "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E147").send().await.unwrap();
+        let ans = parse_resp_to_urls(res).await;
+        assert_eq!(ans, expected);
     }
 }
